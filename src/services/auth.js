104c41db58b5fd50da6e419b260dfbc177316a89
Aneta <anpociecha@gmail.com>
2019-12-04 20:24:11 +0100

    clean up

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,21 @@
+import axios from 'axios'
+
+
 export function signIn(login, password) {
+
+
+    axios.post('http://signmap-backend.herokuapp.com/login?email=jankowalski@gmail.com&password=jan', {})
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
 
     //mock
     return new Promise(function(resolve, reject) {
